d658097d31f6a231485f1ef588bc199c6b5ce23a
Trent <b.trent5@gmail.com>
2017-11-30 19:48:06 -0700

    some more CL tinkering

--- CL.py
+++ CL.py
@@ -1,20 +1,39 @@
 import math
 import random
 
-reference_vectors = []
-input_vectors = []
-clusters = []
+
+def print_vectors(input):
+	for i in range(len(input)):
+		print(str(input[i]))
+	print('\n')
 
 
-def ingest_data():
-	pass
+def random_vectors(n, d):
+	out = []
+	for i in range(n):
+		temp = []
+		for j in range(d):
+			temp.append(random.random())
+		out.append(temp)
+	return out
+
+
+def generate_clustered_data(num_clusters, dimensions, num_data_points):
+	centers = random_vectors(num_clusters, dimensions)
+	data = []
+
+	for i in range(num_data_points):
+		temp = []
+		center = random.randrange(len(centers))
+		for j in range(dimensions):
+			temp.append(random.random() * centers[center][j])
+		data.append(temp)
+	return centers, data
 
 
 def update_winner(input, winner, epsilon):
-	print(str(winner))
 	for i in range(len(winner)):
 		winner[i] += (epsilon * (input[i] - winner[i]))
-	print(str(winner))
 
 def evaluate_clusters():
 	pass
@@ -24,7 +43,6 @@ def euclidian_distance(vector_a, vector_b):
 	if len(vector_a) == len(vector_b):
 		distance = [(a - b)**2 for a, b in zip(vector_a, vector_b)]
 		distance = math.sqrt(sum(distance))
-		print(distance)
 		return distance
 	else :
 		print('Incompatible vectors')
@@ -44,27 +62,36 @@ def compete(input, reference_vectors, epsilon):
 	update_winner(input, reference_vectors[min_index], epsilon)
 
 
-def generate_data(n, len):
-	data = []
-	for i in range(n):
-		temp = []
-		for i in range(len):
-			temp.append(random.randrange(50))
-		data.append(temp)
-	return data
+def train(epsilon, epsilon_step, num_reference_vectors):
+	dimensions = 2
+	num_data_points = 1000
+
+	centers, data = generate_clustered_data(num_reference_vectors, dimensions, num_data_points)
+	print('Actual Centers: ')
+	print_vectors(centers)
+	index = 0
+
+	reference_vectors = random_vectors(num_reference_vectors, dimensions)
+	print('Starting: ')
+	print_vectors(reference_vectors)
+
+	while epsilon > 0:
+		if index >= len(data):
+			index = 0
+		compete(data[index], reference_vectors, epsilon)
+		index += 1
+		epsilon -= epsilon_step
+
+	print('Results')
+	print_vectors(reference_vectors)
+
+	for i in range(len(centers)):
+		print(str(euclidian_distance(centers[i], reference_vectors[i])))
 
 
 def main():
-	# tunables
-	epsilon = .5
-	epsilon_step = 0.01
+	train(.9, 0.000001, 2)
 
-	reference_vectors = [[0, 0], [0, 0]]
-
-	data = generate_data(10, 2)
-
-	for i in range(len(data)):
-		compete(data[i], reference_vectors, epsilon)
 
 if __name__ == '__main__':
 	main()

3f4ec7a2e812856aa101a0c211ea09f4108cc706
BradWhite <white.brad17@gmail.com>
2017-12-08 15:46:18 -0700

    Initialization mostly done, comments

--- ACO.py
+++ ACO.py
@@ -23,21 +23,61 @@ class Ant():
         pass
 
 
-class Datum():
-    def __init__(self, data):
-        self.input_vector = data
-        # TODO get class from last? index of vector
-        self.classification = None
+class ACO():
+    def __init__(self, data, patch_size=1, step_size=1, gammas=[1, 0.01, 0.01]):
+        # Find how large the dataset is and determine the grid size and amount of ants based on that
+        length = len(data)
+        self.grid_size = length * 10
+        # OrderedDict where the keys are tuples for coordinates (x, y), and the value is a dictionary
+        self.grid = self.init_grid(self.grid_size, data)
+        self.ants = self.init_ants(length * 2)
+        # The search space of the ants
+        self.patch_size = patch_size
+        # The amount of spaces an ant can move per move
+        self.step_size = step_size
+        # Tunable parameters for probabilities
+        self.gammas = gammas
+
+    # Initialize the 2D, then place the data in the grid randomly
+    def init_grid(self, dim, data):
+        # Use an ordered dictionary so the points are in the order they are created
+        grid = OrderedDict()
+
+        # Create all the points in the 2D grid and assign its value with a dictionary
+        for x in range(dim + 1):
+            for y in range(dim + 1):
+                grid[(x, y)] = {'Ant': None, 'Datum': None}
+
+        # Place all the data points randomly in the grid
+        for datum in data:
+            # Choose a random location
+            rand_pos = (random.randint(0, self.grid_size), random.randint(0, self.grid_size))
+            # Loop until we have a point in the grid which isn't taken
+            while grid[rand_pos]['Datum'] is not None:
+                rand_pos = (random.randint(0, self.grid_size), random.randint(0, self.grid_size))
+            # Add the datum to the grid
+            grid[rand_pos]['Datum'] = datum
+
+        return grid
+
+    # Create all the ants and place them randomly on the grid
+    def init_ants(self, num_ants):
+        ants = []
+        for i in range(num_ants):
+            # Choose a random location
+            rand_pos = (random.randint(0, self.grid_size), random.randint(0, self.grid_size))
+            # Loop until we have a point in the grid which isn't taken
+            while self.grid[rand_pos]['Ant'] is not None:
+                rand_pos = (random.randint(0, self.grid_size), random.randint(0, self.grid_size))
+            # Create an ant and append it to the list of ants
+            ants.append(Ant(location=rand_pos))
+            # Add the ant's reference to the dictionary
+            self.grid[rand_pos]['Ant'] = ants[i]
+
+        return ants
 
     def similarity(self, point):
         pass
-
-
-class Grid():
-    def __init__(self, dimension, data, patch_size=1, gammas=[1, 0.01, 0.01]):
-        self.grid = None
-        self.patch_size = patch_size
-        self.gammas = gammas
 
     def density(self, ant):
         pass
@@ -48,36 +88,8 @@ class Grid():
     def prob_drop(self, ant):
         pass
 
-
-class ACO():
-    def __init__(self, data, patch_size=1, step_size=1):
-        length = len(data)
-        self.grid_size = length * 10
-        # OrderedDict where the keys are tuples for coordinates (x, y), and the value is a dictionary
-        self.grid = self.init_grid(self.grid_size)
-        self.ants = self.init_ants(length * 2)
-        self.step_size = step_size
-
     def eval_clusters(self):
         pass
-
-    def init_grid(self, dim):
-        grid = OrderedDict()
-
-        for x in range(dim):
-            for y in range(dim):
-                grid[(x, y)] = {'Ant': None, 'Datum': None}
-
-        return grid
-
-    def init_ants(self, num_ants):
-        ants = []
-        # TODO While loop until an open position is found
-        for i in range(num_ants):
-            x = random.randint(0, self.grid_size)
-            y = random.randint(0, self.grid_size)
-            print("x:", x)
-            print("y:", y)
 
     def main(self, max_iter=10000):
         pass
@@ -85,5 +97,4 @@ class ACO():
 
 if __name__ == '__main__':
     aco = ACO(data=[[1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3]])
-    print(aco.grid.keys())
-    print(aco.grid[(25, 25)])
+    # print(aco.grid)

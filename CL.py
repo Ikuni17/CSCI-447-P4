475ddbf9d01a5367091384b20805cfd6533a8c21
trent <b.trent5@gmail.com>
2017-12-09 14:14:39 -0700

    CL should be done

--- CL.py
+++ CL.py
@@ -1,27 +1,25 @@
 import math
 import random
+import numpy as np
+import pandas
 
 
-def print_vectors(input):
+def print_vectors(title, input):
+	print(title)
 	for i in range(len(input)):
 		print(str(input[i]))
 	print('\n')
 
 
-def random_vectors(n, d):
-	out = []
-	for i in range(n):
-		temp = []
-		for j in range(d):
-			temp.append(random.random())
-		out.append(temp)
-	return out
+def print_clusters(clusters):
+	for cluster in clusters.values():
+		print_vectors('Cluster', cluster)
 
 
 def select_random_vectors(n, data):
 	output = []
 	for i in range(n):
-		output.append(data[random.random() * len(data)])
+		output.append(data[int(random.random() * len(data))])
 	return output
 
 
@@ -29,20 +27,15 @@ def update_winner(input, winner, epsilon):
 	for i in range(len(winner)):
 		winner[i] += (epsilon * (input[i] - winner[i]))
 
-def evaluate_clusters():
-	pass
-
 
 def euclidian_distance(vector_a, vector_b):
-	if len(vector_a) == len(vector_b):
-		distance = [(a - b)**2 for a, b in zip(vector_a, vector_b)]
-		distance = math.sqrt(sum(distance))
-		return distance
-	else :
-		print('Incompatible vectors')
+	a = np.array(vector_a)
+	b = np.array(vector_b)
+	# print(str(np.linalg.norm(a-b)))
+	return np.linalg.norm(a-b)
 
 
-def compete(input, reference_vectors, epsilon):
+def compete(input, reference_vectors):
 	min_index = 0
 	min = euclidian_distance(input, reference_vectors[0])
 
@@ -53,8 +46,8 @@ def compete(input, reference_vectors, epsilon):
 			min = temp_distance
 			min_index = i
 
-	update_winner(input, reference_vectors[min_index], epsilon)
-
+	# returns the reference vector with the lowest euclidian distance from the input point
+	return min_index
 
 
 def train(data, num_clusters, epsilon, epsilon_step):
@@ -62,25 +55,45 @@ def train(data, num_clusters, epsilon, epsilon_step):
 	num_data_points = len(data)
 
 	reference_vectors = select_random_vectors(num_clusters, data)
-	print('Starting: ')
+	print_vectors('Starting:', reference_vectors)
 
+	index = 0
 	while epsilon > 0:
 		if index >= len(data):
 			index = 0
-		print_vectors(reference_vectors)
-		compete(data[index], reference_vectors, epsilon)
+		winner = compete(data[index], reference_vectors)
+		update_winner(data[index], reference_vectors[winner], epsilon)
 		index += 1
 		epsilon -= epsilon_step
 
-	print('Results')
-	print_vectors(reference_vectors)
+	# final pass
+	print_vectors('Centers:', reference_vectors)
 
-	for i in range(len(centers)):
-		print(str(euclidian_distance(centers[i], reference_vectors[i])))
+	clusters = {}
+	for point in data:
+		winner_index = compete(point, reference_vectors)
+		if winner_index not in clusters:
+			clusters[winner_index] = [point]
+		else:
+			clusters[winner_index].append(point)
+	print_clusters(clusters)
+
+
+
+
+def read_data(path):
+	df = pandas.read_csv(path, header=None)
+	return df.values.tolist()
 
 
 def main():
-	print(str(generate_random_vectors(4, 2, 10)))
+	data = read_data('datasets/airfoil.csv')
+	num_clusters = 10
+	epsilon = 1
+	epsilon_step = 0.000001
+
+	train(data, num_clusters, epsilon, epsilon_step)
+
 
 if __name__ == '__main__':
 	main()

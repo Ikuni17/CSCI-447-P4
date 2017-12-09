7cd4cabb61c5ede884af1b9a22edbe1f00a79905
Logan Bonney <wakeup2early@gmail.com>
2017-12-09 15:28:50 -0700

    Implemented a functions to generate plot 2d data

--- experiment.py
+++ experiment.py
@@ -6,7 +6,8 @@ December 9, 2017
 
 import ACO
 import pandas
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Read in a csv dataset, convert all values to numbers, and return as a 2D list
 def get_dataset(csv_path):
@@ -26,12 +27,25 @@ def load_datasets():
 
     return datasets
 
+def gen_data():
+    return np.vstack(((np.random.randn(150, 2) * 0.75 + np.array([1, 0])), (np.random.randn(50, 2) * 0.25 + np.array([-0.5, 0.5])), (np.random.randn(50, 2) * 0.5 + np.array([-0.5, -0.5]))))
+
 
 def main():
     datasets = load_datasets()
     aco = ACO.ACO(data=datasets['machine'])
     aco.main(max_iter=1000000)
 
+def graph2dClusters(data):
+    for cluster in data:
+        xVal = [x[0] for x in cluster]
+        yVal = [y[1] for y in cluster]
+        plt.scatter(xVal, yVal, linestyle='None', marker = ".")
+    plt.show()
 
 if __name__ == '__main__':
-    main()
+    #main()
+    graph_data=[] # Putting this into an array to represent a single cluster for testing
+    graph_data.append(gen_data())
+    graph_data.append(gen_data())
+    graph2dClusters(graph_data)

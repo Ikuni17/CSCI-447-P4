2522846dcf0bde7fc5ca6baf45abc9b66c3f3631
trent <b.trent5@gmail.com>
2017-12-09 18:32:12 -0700

    dbscan loading data and syntax

--- DB.py
+++ DB.py
@@ -1,11 +1,14 @@
+import CL
 import KM
+import pandas
 
 minNeighbors = 4
 
 def DBScan(data, minDist):
     clusters = []
     for point in data:
-        clsuters.append(point.getGroup(point, list(data), minDist))
+        clusters.append(getGroup(point, list(data), minDist))
+    return clusters
 
 def getGroup(point, data, minDist):
     group = []
@@ -13,16 +16,28 @@ def getGroup(point, data, minDist):
     for dataPoint in data:
         distance = KM.euclidian_distance(point, dataPoint)
         if distance < minDist and distance > 0:
-            group.append(datapoint)
+            group.append(dataPoint)
             data.remove(dataPoint)
 
-    if(len(group) > 4:
+    if len(group) > 4:
         for groupMember in group:
             group + getGroup(dataPoint, data, minDist)
     else:
         return group
 
-    return neighbors
+    return group
+
+
+def read_data(path):
+    df = pandas.read_csv(path, header=None)
+    return df.values.tolist()
+
 
 if __name__ == '__main__':
-    dbscan = DBScan()
+    data = read_data('datasets/yacht.csv')
+    minDist = 30
+    dbscan = DBScan(data, minDist)
+    for i in dbscan:
+        print('cluster:')
+        for point in i:
+            print(point)

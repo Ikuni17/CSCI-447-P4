658e82df9868f7b110512aa4d70a2f47dff6618f
Brad <white.brad17@gmail.com>
2017-12-09 23:10:20 -0700

    Debugging KM

--- KM.py
+++ KM.py
@@ -1,5 +1,6 @@
 import numpy as np
 from copy import deepcopy
+import math
 import pandas
 import random
 import pprint
@@ -16,9 +17,11 @@ def euclidian_distance(vector_a, vector_b):
 # takes a list of clusters, where a cluster is a list of data points
 def calculate_centroids(clusters):
     centroids = []
+
     for cluster_key in clusters.keys():
         centroids.append(calculate_centroid(clusters[cluster_key]))
 
+    print(centroids)
     return centroids
 
 
@@ -26,30 +29,45 @@ def calculate_centroid(cluster):
     cluster = np.array(cluster)
     cluster_avg = np.zeros_like(cluster)
     for point in cluster:
-        #print(str(point))
+        # print(str(point))
         cluster_avg = np.add(point, cluster_avg)
     cluster_avg = np.divide(cluster_avg, len(cluster))
 
     return cluster_avg
 
 
-def associate_data(centers, data):
+def associate_data(k, centers, data):
     clusters = {}
+    for i in range(k):
+        clusters[i] = []
+
+    # print(centers)
+
+    print("Starting Assoc:", len(centers))
     for point in data:
-        min_index = 0
-        min = euclidian_distance(point, centers[0])
+        min_index = None
+        minimum = math.inf
 
         for center_index in range(len(centers)):
-            temp_distance = euclidian_distance(point, centers[center_index])
-            if temp_distance < min:
-                min = temp_distance
-                min_index = center_index
+            if len(centers[center_index]) == 0:
+                continue
+            else:
+                try:
+                    temp_distance = euclidian_distance(point, centers[center_index])
+                except(ValueError):
+                    print("Point:", point)
+                    print("Center:", centers[center_index])
+                    exit()
+                if temp_distance < minimum:
+                    minimum = temp_distance
+                    min_index = center_index
 
-        if min_index not in clusters:
+        '''if min_index not in clusters:
             clusters[min_index] = [point]
-        else:
-            clusters[min_index].append(point)
+        else:'''
+        clusters[min_index].append(point)
     # pprint.pprint(clusters)
+    print("End Assoc:", len(clusters.keys()))
     return clusters
 
 
@@ -63,20 +81,22 @@ def print_vectors(title, input):
 def train(data, k):
     # select k initial centers randomly from data
     centers = select_random_vectors(k, data)
-    #print_vectors('Centers:', centers)
+    print("Starting Train:", len(centers))
+    # print_vectors('Centers:', centers)
 
     old_centers_hash = hash(str([1]))
     centers_hash = hash(str(centers))
     while old_centers_hash != centers_hash:
         old_centers_hash = hash(str(centers))
 
-        clusters = associate_data(centers, data)
+        clusters = associate_data(k, centers, data)
         centers = calculate_centroids(clusters)
 
         centers_hash = hash(str(centers))
         old_centers = tuple(centers)
-        #print_vectors('Centers:', centers)
+        # print_vectors('Centers:', centers)
 
+    print("End Train:", len(centers))
     return clusters
 
 

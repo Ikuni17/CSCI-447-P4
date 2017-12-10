64208bf70476e5bda299700fec6f324e394808fd
trent <b.trent5@gmail.com>
2017-12-09 18:20:35 -0700

    kmeans, but some of the values are inf

--- KM.py
+++ KM.py
@@ -1,4 +1,7 @@
 import numpy as np
+from copy import deepcopy
+import pandas
+import random
 
 def euclidian_distance(vector_a, vector_b):
 	a = np.array(vector_a)
@@ -8,9 +11,14 @@ def euclidian_distance(vector_a, vector_b):
 
 # takes a list of clusters, where a cluster is a list of data points
 def calculate_centroids(clusters):
-	for cluster in clusters:
-
-	pass
+	centroids = []
+	for cluster in clusters.keys():
+		cluster_avg = np.zeros_like(clusters[cluster])
+		for point in clusters[cluster]:
+			cluster_avg += np.array(point)
+		cluster_avg = np.divide(cluster_avg, clusters[cluster])
+		centroids.append(cluster_avg)
+	return centroids
 
 
 def associate_data(centers, data):
@@ -29,55 +37,28 @@ def associate_data(centers, data):
 			clusters[min_index] = [point]
 		else:
 			clusters[min_index].append(point)
-
 	return clusters
 
 
+def print_vectors(title, input):
+	print(title)
+	for i in input:
+		print(i)
+	print('\n')
+
+
 def train(data, k):
-	converged = False
-	centers = []
-
 	# select k initial centers randomly from data
-	for i in range(k):
-		centers.append(data[int(random.random() * len(data)]))
+	centers = select_random_vectors(k, data)
+	print_vectors('Centers:', centers)
 
+	old_centers = np.zeros_like(centers)
+	while not np.array_equal(centers, old_centers):
+		old_centers = deepcopy(centers)
+		clusters = associate_data(centers, data)
+		centers = calculate_centroids(clusters)
 
-
-		# associate data points with the neatest cluster
-
-
-# -----------------------------------------------------
-	while not converged:
-		converged = True
-
-		# initialize clusters
-		for i in range(len(data):
-			temp_index = None
-			min =
-			for j in range(len(hidden_layer) - 1):
-				# to not include outputs in distance calculation
-				# if min == None or np.linalg.norm(data[i][:len(data[i]) -1] - hidden_layer[j][:len(hidden_layer[j]) - 1]
-				temp_distance = np.linalg.norm(np.array(data[i]) - np.array(hidden_layer[j].center))
-				if temp_distance < min:
-					min = temp_distance
-					temp_index = j
-			clusters[temp_index].append(i)
-
-		# Calculates new centers
-		average = [0] * len(clusters[0])
-		for i in range(len(clusters) - 1):
-			for item in average:
-				item = 0
-			if i == 1:
-				print(average)
-			# For every index in cluster
-			for j in range(len(clusters[i]) - 1):
-				average = [x + y for x, y in zip(average, data[clusters[i][j]])]
-
-			average = [x/len(clusters[i]) for x in average]
-			if average != hidden_layer[i].center:
-				converged = False
-				hidden_layer[i].center = average
+		print_vectors('Centers:', centers)
 
 
 def select_random_vectors(n, data):
@@ -87,8 +68,15 @@ def select_random_vectors(n, data):
 	return output
 
 
+def read_data(path):
+	df = pandas.read_csv(path, header=None)
+	return df.values.tolist()
+
+
 def main():
-	pass
+	data = read_data('datasets/machine.csv')
+	k = 4
+	train(data, k)
 
 if __name__ == '__main__':
 	main()
